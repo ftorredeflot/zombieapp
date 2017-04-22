1fcf2c38733cc42e259e5a340422cca1e8bbc020
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-04-22 13:26:40 +0200

    class zombie

--- src/zombieapp/Zombie.java
+++ src/zombieapp/Zombie.java
@@ -10,5 +10,54 @@ package zombieapp;
  * @author ferran
  */
 public class Zombie {
+    private String name;
+    private int yearOfconversion;
+    private int level;
+    private int[] conversions;
+
+    public Zombie(String name, int yearOfconversion, int level, int[] conversions) {
+        this.name = name;
+        this.yearOfconversion = yearOfconversion;
+        this.level = level;
+        this.conversions = conversions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfconversion() {
+        return yearOfconversion;
+    }
+
+    public void setYearOfconversion(int yearOfconversion) {
+        this.yearOfconversion = yearOfconversion;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int[] getConversions() {
+        return conversions;
+    }
+
+    public void setConversions(int[] conversions) {
+        this.conversions = conversions;
+    }
+
+    @Override
+    public String toString() {
+        return "Zombie{" + "name=" + name + ", yearOfconversion=" + yearOfconversion + ", level=" + level + ", conversions=" + conversions + '}';
+    }
+    
     
 }
