d2e364feaba9e03a57e599a05de35ceee29e111e
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-05-02 19:28:15 +0200

    Construcción de un objeto, zombie2, de tipo Zombie, utilizando el constructor por
    parámetros, los cuales se pedirán al usuario por teclado.

--- src/zombieapp/ZombieAPP.java
+++ src/zombieapp/ZombieAPP.java
@@ -5,7 +5,7 @@ package zombieapp;
  *
  * @author ferran
  */
- import tools.InputData;
+ import tools.InputData.*;
  import zombieapp.Zombie;
 public class ZombieAPP {
 
@@ -15,6 +15,25 @@ public class ZombieAPP {
     public static void main(String[] args) {
         Zombie z1=new Zombie("paco",1994);
         System.out.println("nuevo zombie "+ z1.toString());
+        String nzombie="";
+        nzombie=tools.InputData.pedirCadena("nombre del zombie: ");
+        int yearofc=0;
+        int year1=0;
+        int year2=0;
+        int year3=0;
+        int year4=0;
+        int year5=0;
+        int level;
+        yearofc=tools.InputData.pedirEntero("¿que año se convirtio?");
+        level=tools.InputData.pedirEntero("¿que nivel tiene?");
+        year1=tools.InputData.pedirEntero("¿cuantos convirtio el año 1?");
+        year2=tools.InputData.pedirEntero("¿cuantos convirtio el año 2?");
+       year3=tools.InputData.pedirEntero("¿cuantos convirtio el año 3?");
+        year4=tools.InputData.pedirEntero("¿cuantos convirtio el año 4?");
+        year5=tools.InputData.pedirEntero("¿cuantos convirtio el año 5?");
+        Zombie z2=new Zombie(nzombie,level,yearofc,year1,year2,year3,year4,year5);
+         System.out.println("nuevo zombie "+ z2.toString());
+        
     }
     
 }
