402663bef2688e9b31adfaad0776bd275cf3b592
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-05-02 19:40:53 +0200

    Hola, soy “nombre del zombie”, tengo x años de conversión y me quedan x
    años para convertirme en una bonita flor.

--- src/zombieapp/ZombieAPP.java
+++ src/zombieapp/ZombieAPP.java
@@ -13,7 +13,8 @@ public class ZombieAPP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Zombie z1=new Zombie("paco",1994);
+        int yeara=2016;
+        Zombie z1=new Zombie("paco",2014);
         System.out.println("nuevo zombie "+ z1.toString());
         String nzombie="";
         nzombie=tools.InputData.pedirCadena("nombre del zombie: ");
@@ -24,15 +25,20 @@ public class ZombieAPP {
         int year4=0;
         int year5=0;
         int level;
-        yearofc=tools.InputData.pedirEntero("¿que año se convirtio?");
+       yearofc=tools.InputData.pedirEntero("¿que año se convirtio?");
+        do{
         level=tools.InputData.pedirEntero("¿que nivel tiene?");
+        }while(level<0||level>10);
+       
         year1=tools.InputData.pedirEntero("¿cuantos convirtio el año 1?");
         year2=tools.InputData.pedirEntero("¿cuantos convirtio el año 2?");
        year3=tools.InputData.pedirEntero("¿cuantos convirtio el año 3?");
-        year4=tools.InputData.pedirEntero("¿cuantos convirtio el año 4?");
+       year4=tools.InputData.pedirEntero("¿cuantos convirtio el año 4?");
         year5=tools.InputData.pedirEntero("¿cuantos convirtio el año 5?");
         Zombie z2=new Zombie(nzombie,level,yearofc,year1,year2,year3,year4,year5);
          System.out.println("nuevo zombie "+ z2.toString());
+         
+        System.out.println(" Hola, soy “nombre del zombie”, tengo "+(yeara-z1.getYearOfconversion())+"  años de conversión y me quedan "+(5-(yeara-z1.getYearOfconversion()))+" años para convertirme en una bonita flor.");
         
     }
     
